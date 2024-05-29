3c2bf9436059c786499639119871c5d3442ea06d
Cliff37 <81866031+cliff15037@users.noreply.github.com>
2024-05-29 11:58:59 -0400

    Create solution.cpp

---
+++ leetcode/Array/153. Find Minimum in Rotated Sorted Array/solution.cpp
@@ -0,0 +1,59 @@
+/*
+  This is the solution follows the ordinary binary search method.
+*/
+class Solution {
+public:
+    int findMin(vector<int>& nums) {
+        int l = 0, r = nums.size() - 1;
+
+        while (l <= r) {
+            if (nums[l] <= nums[r]) {  // if the all left to right is ascending
+                return nums[l];
+            }
+
+            int m = l + (r - l) / 2;
+
+            // check if the middle is the smallest
+            if (
+                (m == 0 || nums[m - 1] > nums[m]) && 
+                (m == nums.size() - 1 || nums[m + 1] > nums[m])
+                ) {
+                return nums[m];
+            }
+
+
+            if (nums[m] >= nums[l]) {
+                l = m + 1;  // min in the right
+            } else {
+                r = m - 1;  // min in the left
+            }
+        }
+
+        return -1;  
+    }
+};
+
+
+/*
+  This solution is for this question.
+*/
+class Solution {
+public:
+    int findMin(vector<int>& nums) {
+        int n = nums.size();
+        int l = 0, r = n - 1;
+        
+        while (l < r) {
+            int mid = l + (r - l) / 2;  // prevent overflowing
+            if (nums[mid] < nums[r]) {
+                r = mid;  // min in the mid or mid's left
+            } else {
+                l = mid + 1;  // min in the mid's right
+            }
+        }
+        
+        return nums[l];  // 或 nums[r], 此时 l == r
+    }
+};
+
+
