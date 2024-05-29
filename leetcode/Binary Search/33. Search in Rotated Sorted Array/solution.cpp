b45f3cc655ce76f395fc0df438e990a141f6a285
Cliff37 <81866031+cliff15037@users.noreply.github.com>
2024-05-29 12:17:30 -0400

    Create solution.cpp

---
+++ leetcode/Binary Search/33. Search in Rotated Sorted Array/solution.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int n = nums.size();
+        int l = 0, r = n - 1;
+
+        while (l<=r) {// need to find a specific target, so need to check every element,
+            //use l<=r rather l<r to do so
+            int m = l + (r-l)/2;
+            if (nums[m] == target) {
+                return m;
+            }
+
+            if( nums[m] >= nums[l] ){
+                //l~m is ascending
+                if (nums[l]<target && target<nums[m]){
+                    // target in the left side of m
+                    r = m-1;
+                } else {
+                    l = m+1;
+                }
+            } else {
+                // m ~ r is ascending
+                if (target>nums[m] && target<nums[r]) {
+                    l = m+1;
+                } else {
+                    r = m-1;
+                }
+            }
+        }
+
+        return -1;
+    }
+};
