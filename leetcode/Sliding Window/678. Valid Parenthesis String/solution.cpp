aa9860dcbd43f00565037b2225d29a03dd2aca0a
Tzuyi <81866031+cliff15037@users.noreply.github.com>
2024-06-17 12:05:37 -0400

    Update solution.cpp

--- leetcode/Sliding Window/678. Valid Parenthesis String/solution.cpp
+++ leetcode/Sliding Window/678. Valid Parenthesis String/solution.cpp
@@ -28,3 +28,43 @@ public:
         return min==0;
     }
 };
+
+
+
+
+
+/*
+    Stack method
+*/
+class Solution {
+public:
+    bool checkValidString(string s) {
+        stack<int> leftStack, starStack;
+
+        for (int i = 0; i < s.size(); ++i) {
+            if (s[i] == '(') {
+                leftStack.push(i);
+            } else if (s[i] == '*') {
+                starStack.push(i);
+            } else { // s[i] == ')'
+                if (!leftStack.empty()) {
+                    leftStack.pop();
+                } else if (!starStack.empty()) {
+                    starStack.pop();
+                } else {
+                    return false;
+                }
+            }
+        }
+
+        while (!leftStack.empty() && !starStack.empty()) {
+            if (leftStack.top() > starStack.top()) {
+                return false;
+            }
+            leftStack.pop();
+            starStack.pop();
+        }
+
+        return leftStack.empty();
+    }
+};
